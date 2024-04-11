ef910a82083c7603f2659f71e88f8e92ef692272
iambehzad <behzad.abbasy@gmail.com>
2024-04-11 17:56:07 +0330

    commit for DB exc
    تا تمرین مباحث پایگاه‌داده و ادمین - بخش اول

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy='created_date'
+    empty_value_display = "-empty-"
+    #fields=('title', )
+    #exclude('created_date')
+    list_display=('title', 'counted_view', 'status', 'published_date', 'created_date')
+    list_filter=('status', )
+    #prepopulated_fields
+    #ordering=['-created_date']
+    search_fields=('title','content',)
+
+admin.site.register(Post,PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 def blog_view(request):
-    return render (request, 'blog/blog-home.html')
-def blog_single(request):
-    context = {'title': 'bitcoin crashed again!', 'content':'bitcoin was flying but now grounded as always', 'author': 'Behzad Abbasi'}
+    current_time = timezone.now()
+    posts = Post.objects.filter(published_date__lte=current_time)
+    context = {'posts': posts}
+    return render (request, 'blog/blog-home.html',context)
+
+def blog_single(request, pid):
+    post = get_object_or_404(Post,pk=pid)
+    post.counted_view += 1
+    post.save()
+    context = {'post': post}
     return render (request, 'blog/blog-single.html', context)
