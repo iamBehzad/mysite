6ba2796e54682a8c003f3a4a12e420acc9c26291
iambehzad <behzad.abbasy@gmail.com>
2024-04-17 17:41:40 +0330

    Add published_date  in latest posts
    Add published_date in latest posts

--- blog/templatetags/latest_posts.py
+++ blog/templatetags/latest_posts.py
@@ -1,9 +1,11 @@
 from django import template
 from blog.models import Post
+from django.utils import timezone
 
 register = template.Library()
 
 @register.inclusion_tag('blog/latest_posts.html')
 def show_latest_posts(count=6):
-    latest_posts = Post.objects.filter(status =1).order_by('-published_date')[:count]
+    current_time = timezone.now()
+    latest_posts = Post.objects.filter(status =1,published_date__lte=current_time).order_by('-published_date')[:count]
     return {'latest_posts': latest_posts}
