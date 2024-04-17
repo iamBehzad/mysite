bc6d4142a40502423b27afa13b2ab82e05082d06
iambehzad <behzad.abbasy@gmail.com>
2024-04-17 16:54:56 +0330

    update template tag and db
    update template tag and db

--- blog/templatetags/latest_posts.py
+++ blog/templatetags/latest_posts.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 @register.inclusion_tag('blog/latest_posts.html')
 def show_latest_posts(count=6):
-    latest_posts = Post.objects.order_by('-published_date')[:count]
+    latest_posts = Post.objects.filter(status =1).order_by('-published_date')[:count]
     return {'latest_posts': latest_posts}
