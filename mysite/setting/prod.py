950d4bd258d7681402289331c88beb19d00db91e
iambehzad <behzad.abbasy@gmail.com>
2024-05-17 19:29:15 +0330

    last commit 14020228
    last commit 14020228

--- mysite/setting/prod.py
+++ mysite/setting/prod.py
@@ -13,15 +13,15 @@ ALLOWED_HOSTS = []
 #INSTALLED_APPS = []
 
 # site framework
-SITE_ID = 2
+SITE_ID = 1
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR /'my-db',
     }
 }
 
